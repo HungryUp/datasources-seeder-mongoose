9881d08dd7a8c743b08cf35db58583042dab66ab
Gaël <gael@hungryup.co>
2017-09-21 15:11:18 +0200

    Here we go !

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # datasources-seeder-mongoose
 Driver mongoose for datasources-seeder
+
+    "mongo":  {
+      "driver": "datasources-seeder-mongoose",
+      "uri": "mongodb://localhost/test",
+      "models": "models",
+      "path": "seeds/mongo/",
+      "destroyOld": true
+    }
---
+++ index.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const { promisify } = require('util');
+const fs = require('fs');
+const { join } = require('path');
+const readdir = promisify(fs.readdir);
+
+module.exports = {
+  async seed(db, { path }, contents = []) {
+    return Promise.all(contents
+      .filter(file => ~file.search(/^[^.].*\.json$/))
+      .map((file) => {
+        const jsonPath = join(process.cwd(), path, file);
+        const [modelName] = file.split('.');
+        const json = require(jsonPath);
+        const Model = mongoose.model(modelName);
+        return Promise.all(
+          json.map(document => {
+            const object = new Model(document);
+            return object.save()
+          })
+        );
+      }));
+  },
+
+  async init({ uri, models, destroyOld = false } = {}) {
+    mongoose.Promise = Promise;
+    mongoose.connection.on('error', console.error);
+    const db = await mongoose.connect(uri);
+    if (destroyOld) {
+      await mongoose.connection.db.dropDatabase();
+    }
+    const modelsPath = join(process.cwd(), models);
+    (await readdir(modelsPath))
+      .filter(file => ~file.search(/^[^.].*\.js$/))
+      .map(modelName => require(join(modelsPath, modelName)))
+      .filter(model => !!model.schema)
+      .forEach(model => mongoose.model(model.modelName, model.schema));
+    return db;
+  },
+
+  async end() {
+    return mongoose.disconnect();
+  }
+};
